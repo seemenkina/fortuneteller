e9e928d6e87fafb6b9bf3c7ade874a6f4411d9b5
Ekaterina <seemenkina@gmail.com>
2021-04-17 21:35:06 +0700

    fix test

--- internal/repository/book_test.go
+++ internal/repository/book_test.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"context"
 	"testing"
 
 	"fortuneteller/internal/data"
@@ -11,7 +10,7 @@ import (
 
 func TestBookfs_ListBooks(t *testing.T) {
 	repo := NewBookFileSystem("testdata/books")
-	books, err := repo.ListBooks(context.TODO())
+	books, err := repo.ListBooks()
 	require.NoError(t, err)
 	assert.ElementsMatch(t, books, []data.Book{
 		{"b1", 5},
@@ -20,14 +19,14 @@ func TestBookfs_ListBooks(t *testing.T) {
 
 func TestBookfs_FindRowInBook(t *testing.T) {
 	repo := NewBookFileSystem("testdata/books")
-	str, err := repo.FindRowInBook(context.TODO(), "b1", 3)
+	str, err := repo.FindRowInBook("b1", 3)
 	require.NoError(t, err)
 	assert.Equal(t, str, "4")
 }
 
 func TestBookfs_GetBookKey(t *testing.T) {
 	repo := NewBookFileSystem("testdata/books")
-	bk, err := repo.GetBookKey(context.TODO(), "b1")
+	bk, err := repo.GetBookKey("b1")
 	require.NoError(t, err)
 	c, err := bk.Encrypt([]byte("FY0363251JDF9IC02BPFX245C3FCD66="))
 	require.NoError(t, err)
